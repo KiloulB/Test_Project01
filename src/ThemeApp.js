570eccbc0299b58590a00e1d39eb3801f210fbbc
KiloulB <kiloulb@gmail.com>
2021-09-01 17:46:14 +0200

    c3

--- src/ThemeApp.js
+++ src/ThemeApp.js
@@ -1,31 +1,36 @@
-import baseTheme from  './styles/MUItheme'
+import baseTheme from './styles/MUItheme'
 import { ThemeProvider, createMuiTheme, CssBaseline } from '@material-ui/core'
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
 import { useState } from 'react';
 import App from './App'
 
 function ThemeApp() {
-  const [MUITheme, setMUITheme] = useState(createMuiTheme(baseTheme)); 
-  const fsHandle = useFullScreenHandle();
+    const [MUITheme, setMUITheme] = useState(createMuiTheme(baseTheme));
+    const fsHandle = useFullScreenHandle();
 
-  const changeColor = (color) => {
-    setMUITheme(createMuiTheme({
-      ...baseTheme,
-      palette: {
-        ...baseTheme.palette,
-        ...color
-      }
-    }))
-  }
-  
-  return (
-      <ThemeProvider theme={MUITheme}>
-        <CssBaseline/>
-        <FullScreen handle={fsHandle}>
-          <App changeColor={changeColor} fsHandle={fsHandle}/>
-        </FullScreen>
-      </ThemeProvider>
-  );
+    const changeColor = (color) => {
+        setMUITheme(createMuiTheme({
+            ...baseTheme,
+            palette: {
+                ...baseTheme.palette,
+                ...color
+            }
+        }))
+    }
+
+    return ( <
+        ThemeProvider theme = { MUITheme } >
+        <
+        CssBaseline / >
+        <
+        FullScreen handle = { fsHandle } >
+        <
+        App changeColor = { changeColor }
+        fsHandle = { fsHandle }
+        /> <
+        /FullScreen> <
+        /ThemeProvider>
+    );
 }
 
 export default ThemeApp;
